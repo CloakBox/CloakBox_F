20f5655154de470cb5c0e80d7bd006a2ae0e23c1
zony <hjsim@ibleaders.co.kr>
2025-07-11 14:09:29 +0900

    Router 설정 추가

---
+++ src/login/Login.tsx
@@ -0,0 +1,7 @@
+/**
+ * 로그인 화면
+ * @constructor
+ */
+export const Login = () => {
+  return <div>Login</div>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Login} from "./login/Login.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={ <App />}></Route>
+              <Route path="/login" element={ <Login />}></Route>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
