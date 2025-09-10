81f452e1e5abf260c1706b8acd6d4f9621562636
zony <hjsim@ibleaders.co.kr>
2025-09-10 14:58:18 +0900

    메인화면 slider 추가

--- src/main/content/TitleContent.tsx
+++ src/main/content/TitleContent.tsx
@@ -1,12 +1,30 @@
 import {ViewBox} from "./element/ViewBox.tsx";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export const  TitleContent = () => {
-    return(
+
+export const TitleContent = () => {
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "50px",
+        slidesToShow: 3,
+        speed: 500
+    };
+
+    return (
         <>
-            <div className="main_content .d-flex">
-                <ViewBox/>
-                <ViewBox/>
-                <ViewBox/>
+            <div className="main_content">
+                <div className="slider-container">
+                    <Slider {...settings}>
+                        <ViewBox/>
+                        <ViewBox/>
+                        <ViewBox/>
+                        <ViewBox/>
+                    </Slider>
+                </div>
             </div>
         </>
     )
--- src/main/content/element/ViewBox.tsx
+++ src/main/content/element/ViewBox.tsx
@@ -1,15 +1,15 @@
 export const ViewBox = () => {
 
     return (
-        <div className="container border-5 rounded-3 border-warning viewbox_content">
-            <div className="row viewbox_title fw-bold">
+        <div className="border-5 rounded-3 border-warning viewbox_content">
+            <div className="viewbox_title fw-bold">
                 타이틀 타이틀 타이틀
             </div>
-            <div className="row viewbox_under">
-                <div className="row viewbox_content__title">
+            <div className="viewbox_under">
+                <div className="viewbox_content__title">
                     카테고리카테고리카테고리
                 </div>
-                <div className="row viewbox_content__title">
+                <div className="viewbox_content__title">
                     날짜 날짜 날짜
                 </div>
             </div>
